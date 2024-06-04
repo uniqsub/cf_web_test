53e585e11c1a7c09bc98dc60167fb23539a68a5a
uniq <reis.sugiyama0428+sub@gmail.com>
2024-06-05 01:50:09 +0900

    Add files via upload

--- js/load.js
+++ js/load.js
@@ -9,11 +9,11 @@ $(function () {
       $('.loader .txt').fadeIn(400);
     }
     // テキスト表示2
-    function show_txt() {
+    function show_txt2() {
         $('.loader .txt2').fadeIn(400);
       }
     // テキスト表示3
-    function show_txt() {
+    function show_txt3() {
         $('.loader .txt3').fadeIn(400);
       }
     // テキスト非表示
@@ -21,37 +21,37 @@ $(function () {
       $('.loader .txt').fadeOut(400);
     }
     // テキスト非表示2
-    function hide_txt() {
+    function hide_txt2() {
         $('.loader .txt2').fadeOut(400);
      }
     // テキスト非表示3
-    function hide_txt() {
+    function hide_txt3() {
         $('.loader .txt3').fadeOut(400);
       }
    
     // タイマー処理
     $(window).on('load', function () {
-      // 処理①
+      // 処理①1
       setTimeout(function () {
         show_txt();
       }, 1000)
-    // 処理①
+    // 処理①2
     setTimeout(function () {
         show_txt2();
         }, 2000)
-    // 処理①
+    // 処理①3
       setTimeout(function () {
         show_txt3();
       }, 3000)
-      // 処理②
+      // 処理②1
       setTimeout(function () {
         hide_txt();
       }, 5500)
-      // 処理②
+      // 処理②2
       setTimeout(function () {
         hide_txt2();
       }, 5500)
-      // 処理②
+      // 処理②3
       setTimeout(function () {
         hide_txt3();
       }, 5500)
