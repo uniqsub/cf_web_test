af1af3e3eb2641c9955a0cec5c3bbc9e554c5c0d
uniq <reis.sugiyama0428+sub@gmail.com>
2024-06-06 23:36:45 +0900

    latest

--- js/load.js
+++ js/load.js
@@ -12,10 +12,6 @@ $(function () {
     function show_txt2() {
         $('.loader .txt2').fadeIn(400);
       }
-    // テキスト表示3
-    function show_txt3() {
-        $('.loader .txt3').fadeIn(400);
-      }
     // テキスト非表示
     function hide_txt() {
       $('.loader .txt').fadeOut(400);
@@ -24,10 +20,6 @@ $(function () {
     function hide_txt2() {
         $('.loader .txt2').fadeOut(400);
      }
-    // テキスト非表示3
-    function hide_txt3() {
-        $('.loader .txt3').fadeOut(400);
-      }
    
     // タイマー処理
     $(window).on('load', function () {
@@ -39,25 +31,21 @@ $(function () {
     setTimeout(function () {
         show_txt2();
         }, 2000)
-    // 処理①3
-      setTimeout(function () {
-        show_txt3();
-      }, 3000)
       // 処理②1
       setTimeout(function () {
         hide_txt();
-      }, 5500)
+      }, 4500)
       // 処理②2
       setTimeout(function () {
         hide_txt2();
-      }, 5500)
+      }, 4500)
       // 処理②3
       setTimeout(function () {
         hide_txt3();
-      }, 5500)
+      }, 4500)
       // 処理③
       setTimeout(function () {
         end_loader();
-      },6500)
+      },5500)
     })
   })
