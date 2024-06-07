19dfb69f866e352311174a56f46ef3e6b81f556f
uniq <reis.sugiyama0428+sub@gmail.com>
2024-06-07 16:39:30 +0900

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,10 @@
-$(function(){
-	// 変数に要素を入れる
-	var open = $('.modal-open'),
-		close = $('.modal-close'),
-		container = $('.modal-container');
-
-	//開くボタンをクリックしたらモーダルを表示する
-	open.on('click',function(){	
-		container.addClass('active');
-		return false;
-	});
-
-	//閉じるボタンをクリックしたらモーダルを閉じる
-	close.on('click',function(){	
-		container.removeClass('active');
-	});
-
-	//モーダルの外側をクリックしたらモーダルを閉じる
-	$(document).on('click',function(e) {
-		if(!$(e.target).closest('.modal-body').length) {
-			container.removeClass('active');
-		}
-	});
+$(function() {
+  //モーダルを開く
+  $('.modalBtn').click(function() {
+    $('.overlay, .modal').fadeIn();
+  })
+  //モーダルの外側か閉じるをクリックでモーダルを閉じる
+  $('.overlay, .close').click(function() {
+    $('.overlay, .modal').fadeOut();
+  })
 });
